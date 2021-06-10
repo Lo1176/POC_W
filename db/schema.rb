d798e1ac45d47835c462cd71fe3baaa1a40e8f3d
Laurent Binder <laurentbinder@gmail.com>
2021-06-10 14:42:20 +0200

    changed routes && db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_10_111937) do
+ActiveRecord::Schema.define(version: 2021_06_10_120225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "shops", force: :cascade do |t|
+    t.string "shopify_domain", null: false
+    t.string "shopify_token", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "access_scopes"
+    t.index ["shopify_domain"], name: "index_shops_on_shopify_domain", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
